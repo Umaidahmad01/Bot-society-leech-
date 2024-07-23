e096f3ea3c8a7de00dda6628be872383cdb1f605
Umaidahmad01 <ahmedumaid707@gmail.com>
2024-07-23 17:40:26 +0530

    Update __init__.py

--- tobrot/__init__.py
+++ tobrot/__init__.py
@@ -48,14 +48,14 @@ for imp in ["TG_BOT_TOKEN", "APP_ID", "API_HASH", "OWNER_ID", "AUTH_CHANNEL"]:
         exit()
 
 # The Telegram API things
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-APP_ID = int(os.environ.get("APP_ID", "12345"))
-API_HASH = os.environ.get("API_HASH")
-OWNER_ID = int(os.environ.get("OWNER_ID", "539295917"))
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7453828205:AAE3nZi4LEpBfuXA9aUOMeXh8_yWERPTJBo")
+APP_ID = int(os.environ.get("APP_ID", "20718334"))
+API_HASH = os.environ.get("API_HASH"4e81464b29d79c58d0ad8a0c55ece4a5")
+OWNER_ID = int(os.environ.get("OWNER_ID", "5585016974"))
 
 # Get these values from my.telegram.org
 # to store the channel ID who are authorized to use the bot
-AUTH_CHANNEL = [int(x) for x in os.environ.get("AUTH_CHANNEL", "539295917").split()]
+AUTH_CHANNEL = [int(x) for x in os.environ.get("AUTH_CHANNEL", "-1001930406310").split()]
 
 # the download location, where the HTTP Server runs
 DOWNLOAD_LOCATION = "./DOWNLOADS"
